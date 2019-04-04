d5348cf9888c97a578fffcfa507c54093e1ca61b
natashabornhorst <nbornho1@jhu.edu>
2019-04-04 10:55:08 -0400

    submit review

--- GoodSideApp/navigation/MainTabNavigator.js
+++ GoodSideApp/navigation/MainTabNavigator.js
@@ -30,7 +30,7 @@ const LinksStack = createStackNavigator({
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: 'Submit Review',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
@@ -44,7 +44,7 @@ const SettingsStack = createStackNavigator({
 });
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+  tabBarLabel: 'Review',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
--- GoodSideApp/screens/SettingsScreen.js
+++ GoodSideApp/screens/SettingsScreen.js
@@ -1,14 +1,134 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { Input, Icon } from 'react-native-elements'
+import { 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  Button, 
+  View, 
+  AppRegistry, 
+  Image, 
+  TouchableOpacity } from 'react-native';
+import { Google } from 'expo';
+import { Constants, SQLite } from 'expo';
 
-export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
+const db = SQLite.openDatabase('db.db');
+
+export default class SignUpScreen extends React.Component {
+  state = {
+    name: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  handleBio = (text) => {
+    this.setState({ bio: text })
+  }
+  handlePic = (text) => {
+    this.setState({ pic: text })
+  }
+
+  submit = (bio, pic) => {
+        // is text empty?
+    if (bio === null || bio === '') {
+      alert('Please enter a bio')
+      return false;
+    } else if (pic === null || pic === '') {
+      alert('Please choose a pic')
+      return false;
+    } 
+
+    db.transaction(
+      tx => {
+        tx.executeSql('insert into reviews (bio, pic) values (?, ?)', [bio, pic]);
+        tx.executeSql('select * from reviews', [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      },
+      null,
+      this.update
+    );
+    this.props.navigation.navigate('Main');
+  }
+
+  componentDidMount() {
+    db.transaction(tx => {
+      tx.executeSql(
+        'create table if not exists reviews (id integer primary key not null, bio text, pic text);'
+      );
+    });
+  }
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.top}>
+            <Text style={styles.header}> review </Text>
+        </View>
+
+        <View style={styles.bottom}>
+          <Input onChangeText = {this.handleBio} containerStyle={styles.inputField} shake={true} placeholder='bio' />
+          <Input onChangeText = {this.handlePic} containerStyle={styles.inputField} shake={true} placeholder='pic' />
+          <TouchableOpacity 
+            onPress = {() => this.submit(this.state.bio, this.state.pic)}
+            style={styles.button}>
+            <Text style={styles.text}> next </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
+AppRegistry.registerComponent('GoodSide', () => FlexDimensionsBasics);
+AppRegistry.registerComponent('GoodSide', () => DisplayAnImage);
+AppRegistry.registerComponent('GoodSide', () => JustifyContentBasics);
+
+const styles = StyleSheet.create({
+  top: {
+    flex: 1,
+    backgroundColor: '#f8cc1f',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50
+  },
+  bottom: {
+    flex: 3,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputField: {
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#f8cc1f',
+    width: 300,
+    height: 50,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonWhite: {
+    backgroundColor: '#fff',
+    width: 300,
+    height: 50,
+    borderRadius: 20,
+    borderWidth: 0.5,
+    borderColor: '#f8cc1f',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  text: {
+    color: '#fff',
+  },
+  header: {
+    color: '#fff',
+    fontSize: 30
+  },
+  textYellow: {
+    color: '#f8cc1f',
+  },
+});
