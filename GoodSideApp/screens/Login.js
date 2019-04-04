ee83ba795d2d16759cee57fdb85ea459bae5718d
natashabornhorst <nbornho1@jhu.edu>
2019-04-03 22:06:59 -0400

    added sqlite database and signup/login work

--- GoodSideApp/screens/Login.js
+++ GoodSideApp/screens/Login.js
@@ -27,8 +27,57 @@ export default class Login extends React.Component {
     this.setState({ password: text })
   }
 
-  login = (username, password) => {
-    alert('username: ' + username + ' password: ' + password)
+  signup = (username, password) => {
+        // is text empty?
+    if (username === null || username === '' || password === null || password === '') {
+      return false;
+    }
+
+    db.transaction(
+      tx => {
+        tx.executeSql('insert into users (username, password) values (?, ?)', [username, password]);
+        tx.executeSql('select * from users', [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      },
+      null,
+      this.update
+    );
+  }
+
+  signin = (username, password) => {
+        // is text empty?
+    if (username === null || username === '') {
+      alert('Please enter a username')
+      return false;
+    } else if (password === null || password === '') {
+      alert('Please enter a password')
+      return false;
+    }
+
+    db.transaction(
+      tx => {
+        tx.executeSql('select * from users where username = ? and password = ?', [username, password], (tx, results) => {
+          var length = results.rows.length;
+          console.log('length: ', length);
+          if (length > 0) {
+            this.props.navigation.navigate('Main');
+          } else {
+            alert('Wrong username or password');
+          }
+        })
+      },
+      null,
+      this.update
+    );
+  }
+
+  componentDidMount() {
+    db.transaction(tx => {
+      tx.executeSql(
+        'create table if not exists users (id integer primary key not null, username text, password text);'
+      );
+    });
   }
 
   render() {
@@ -45,12 +94,12 @@ export default class Login extends React.Component {
           <Input onChangeText = {this.handleUsername} containerStyle={styles.inputField} shake={true} placeholder='email' rightIcon={{ type: 'font-awesome', name: 'envelope', color: '#f8cc1f' }}/>
           <Input onChangeText = {this.handlePassword} containerStyle={styles.inputField} shake={true} placeholder='password' rightIcon={{ type: 'font-awesome', name: 'lock', color: '#f8cc1f', size: 30 }}/>
           <TouchableOpacity 
-            onPress = {() => this.login(this.state.username, this.state.password)}
+            onPress = {() => this.signin(this.state.username, this.state.password)}
             style={styles.button}>
             <Text style={styles.text}> sign in </Text>
           </TouchableOpacity>
           <TouchableOpacity 
-            onPress = {() => this.login(this.state.username, this.state.password)}
+            onPress = {() => this.signup(this.state.username, this.state.password)}
             style={styles.buttonWhite}>
             <Text style={styles.textYellow}> sign up </Text>
           </TouchableOpacity>
